ba3bad85913146b44d0137faf2ef59ec13e0c624
Manish Daksh <manishk78625@gmail.com>
2024-12-15 11:10:24 +0530

    initial commit

---
+++ src/components/button.jsx
@@ -0,0 +1,16 @@
+
+
+
+
+    const sharebtn = {
+        title : "Share",
+        startIcon : <shareIcon/>
+        
+    }
+    export function Buttons({title,startIcon}:){
+    return (
+        <button>
+            text
+        </button>
+    )
+} 
